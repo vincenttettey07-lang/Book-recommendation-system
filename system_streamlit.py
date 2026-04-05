fcd27f445da00148859502bac483a8f2f876fc32
vincenttettey07-lang <vincenttettey07@gmail.com>
2026-04-05 20:52:36 +0000

    Add error handling for book not found
    Handle exceptions when a book is not found and display an error message.

--- system_streamlit.py
+++ system_streamlit.py
@@ -195,6 +195,11 @@ if st.button("Recommend"):
                 st.image(row['Image-URL-M'])
                 st.write(row['Book-Title'])
 
+except Exception:
+    st.error("😮Book not found)
+
+
+
     
     
     
